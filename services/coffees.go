cd0891bde6cde7d7bd65d1422e078062ead45920
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2022-02-10 18:07:35 -0300

    add coffee services

---
+++ services/coffees.go
@@ -0,0 +1,32 @@
+package services
+
+import (
+	"menu/database"
+	"menu/models"
+)
+
+func GetCoffees() []models.Coffee {
+	var coffees []models.Coffee
+	database.Connect().Find(&coffees)
+	return coffees
+}
+
+func GetCoffee(id int) models.Coffee {
+	var coffee models.Coffee
+	database.Connect().First(&coffee, id)
+	return coffee
+}
+
+func CreateCoffee(coffee models.Coffee) models.Coffee {
+	database.Connect().Create(&coffee)
+	return coffee
+}
+
+func UpdateCoffee(id int, coffee models.Coffee) models.Coffee {
+	database.Connect().Model(&coffee).Where("id = ?", id).Updates(coffee)
+	return coffee
+}
+
+func DeleteCoffee(id int) {
+	database.Connect().Delete(&models.Coffee{}, id)
+}
