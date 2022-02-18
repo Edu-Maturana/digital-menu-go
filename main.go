bb5e1a5fb1f8dd4fe7a32195eebc2da901052bcb
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2022-02-18 20:08:35 -0300

    delete employees

--- main.go
+++ main.go
@@ -19,11 +19,6 @@ func main() {
 	router.HandleFunc("/coffees/{id}", handlers.UpdateCoffee).Methods("PUT")
 	router.HandleFunc("/coffees/{id}", handlers.DeleteCoffee).Methods("DELETE")
 
-	// Employee endpoints
-	router.HandleFunc("/employees", handlers.CreateEmployee).Methods("POST")
-	router.HandleFunc("/employees/{id}", handlers.UpdateEmployee).Methods("PUT")
-	router.HandleFunc("/employees/{id}", handlers.DeleteEmployee).Methods("DELETE")
-
 	fmt.Println("Server running!")
 	http.ListenAndServe(":3000", router)
 }
